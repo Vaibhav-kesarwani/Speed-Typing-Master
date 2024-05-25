2d4fbcb68cd1f9e1a19f867a2b9fcc717faff77a
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-25 10:41:05 +0530

    format the generated word file

--- src/components/GeneratedWords.tsx
+++ src/components/GeneratedWords.tsx
@@ -1,11 +1,5 @@
 const GeneratedWords = ({ words }: { words: string }) => {
-  return (
-    <div
-      className="text-slate-600"
-    >
-      {words}
-    </div>
-  );
+  return <div className="text-slate-600">{words}</div>;
 };
 
 export default GeneratedWords;
