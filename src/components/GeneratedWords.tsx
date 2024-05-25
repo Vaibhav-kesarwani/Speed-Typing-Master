0d62e20102ca4b7ca02b41dc7edf97010aee4ed5
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-25 10:40:27 +0530

    remove the redant code

--- src/components/GeneratedWords.tsx
+++ src/components/GeneratedWords.tsx
@@ -2,7 +2,6 @@ const GeneratedWords = ({ words }: { words: string }) => {
   return (
     <div
       // initial={{ opacity: 0 }}
-      // animate={{ opacity: 1, transition: { duration: 0.01 } }}
       className="text-slate-600"
     >
       {words}
