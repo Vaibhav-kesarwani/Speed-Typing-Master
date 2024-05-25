11c5ce88295fd723cda4de83b21192f5e4038d1c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-25 12:37:42 +0530

    remove the redant code and also change the string slate color

--- src/components/GeneratedWords.tsx
+++ src/components/GeneratedWords.tsx
@@ -1,14 +1,5 @@
 const GeneratedWords = ({ words }: { words: string }) => {
-  return (
-    <div
-      // initial={{ opacity: 0 }}
-      // animate={{ opacity: 1, transition: { duration: 0.01 } }}
-      // exit={{ opacity: 0 }}
-      className="text-slate-500"
-    >
-      {words}
-    </div>
-  );
+  return <div className="text-slate-600">{words}</div>;
 };
 
 export default GeneratedWords;
