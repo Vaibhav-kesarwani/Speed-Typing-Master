f26827f3cfd18a8f45c6a6fdc0c23e4343037c2c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-05-25 12:33:20 +0530

    change the stash fies

--- src/components/GeneratedWords.tsx
+++ src/components/GeneratedWords.tsx
@@ -1,5 +1,14 @@
 const GeneratedWords = ({ words }: { words: string }) => {
-  return <div className="text-slate-600">{words}</div>;
+  return (
+    <div
+      // initial={{ opacity: 0 }}
+      // animate={{ opacity: 1, transition: { duration: 0.01 } }}
+      // exit={{ opacity: 0 }}
+      className="text-slate-500"
+    >
+      {words}
+    </div>
+  );
 };
 
 export default GeneratedWords;
